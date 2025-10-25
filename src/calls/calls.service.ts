b0eda299ae5d99359c674486499bbb62921c6a5e
jes11sy <defouz03@gmail.com>
2025-10-25 15:32:46 +0400

    321

--- src/calls/calls.service.ts
+++ src/calls/calls.service.ts
@@ -304,7 +304,7 @@ export class CallsService {
 
   async getCallsByOrderId(orderId: number) {
     // Получаем заказ
-    const order = await this.prisma.orders.findUnique({
+    const order = await this.prisma.order.findUnique({
       where: { id: orderId }
     });
 
